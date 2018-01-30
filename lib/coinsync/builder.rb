2b5b5feb491a9ae7097da965571d9043a56604f2
Kuba Suder <jakub.suder@gmail.com>
2018-01-30 13:04:16 +0000

    allow setting custom column separator

--- lib/coinsync/builder.rb
+++ lib/coinsync/builder.rb
@@ -32,7 +32,7 @@ module CoinSync
         block = proc { |tx, csv| csv << formatter.save_to_csv(tx) }
       end
 
-      CSV.open(filename, 'w', col_sep: ';') do |csv|
+      CSV.open(filename, 'w', col_sep: @settings['column_separator'] || ',') do |csv|
         transactions.sort_by { |tx| tx.time }.each_with_index do |tx, i|
           tx.number = i + 1
           block.call(tx, csv)
