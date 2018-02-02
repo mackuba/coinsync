bbb490bdede55ed851088b92323a24d50fd6e21d
Kuba Suder <jakub.suder@gmail.com>
2018-02-02 03:02:52 +0100

    added summary output listing how much you should have

--- lib/coinsync.rb
+++ lib/coinsync.rb
@@ -1,6 +1,7 @@
 require "coinsync/builder"
 require "coinsync/config"
 require "coinsync/outputs/default"
+require "coinsync/outputs/summary"
 require "coinsync/version"
 
 module CoinSync
---
+++ lib/coinsync/outputs/summary.rb
@@ -0,0 +1,33 @@
+require_relative '../currencies'
+
+module CoinSync
+  module Outputs
+    class Summary
+      def process_transactions(transactions)
+        totals = Hash.new(0)
+
+        transactions.each do |tx|
+          if tx.bought_currency.is_a?(CryptoCurrency)
+            amount = totals[tx.bought_currency]
+            totals[tx.bought_currency] = amount + tx.bought_amount
+          end
+
+          if tx.sold_currency.is_a?(CryptoCurrency)
+            amount = totals[tx.sold_currency]
+            if amount >= tx.sold_amount
+              totals[tx.sold_currency] = amount - tx.sold_amount
+            else
+              raise "Summary: couldn't sell #{tx.sold_amount} #{tx.sold_currency.code} if only #{amount} was owned"
+            end
+          end
+        end
+
+        max_len = totals.keys.map(&:code).map(&:length).max
+
+        totals.each do |currency, amount|
+          puts (currency.code + ":").ljust(max_len + 1) + "  #{amount}"
+        end
+      end
+    end
+  end
+end
