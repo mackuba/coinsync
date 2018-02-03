a3881a80578bea51dcdd0798b218554f0ed93ae6
Kuba Suder <jakub.suder@gmail.com>
2018-02-03 22:02:47 +0100

    use constant 2 decimal digits for fiat formatting

--- lib/coinsync/number_formatter.rb
+++ lib/coinsync/number_formatter.rb
@@ -5,19 +5,19 @@ module CoinSync
       @decimal_separator = config.custom_decimal_separator
     end
 
-    def format_float(value, precision:)
+    def format_float(value, precision:, trailing_zeros: true)
       s = sprintf("%.#{precision}f", value)
-      s = s.gsub(/0+$/, '').gsub(/\.$/, '')
+      s = s.gsub(/0+$/, '').gsub(/\.$/, '') unless trailing_zeros
       s = s.gsub(/\./, @decimal_separator) if @decimal_separator
       s
     end
 
     def format_fiat(amount)
-      format_float(amount, precision: 4)
+      format_float(amount, precision: 2, trailing_zeros: true)
     end
 
     def format_crypto(amount)
-      format_float(amount, precision: 8)
+      format_float(amount, precision: 8, trailing_zeros: false)
     end
   end
 end
