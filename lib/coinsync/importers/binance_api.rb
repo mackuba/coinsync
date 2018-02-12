8b08d27b8ff98bddd1451e269372c3646205d76f
Kuba Suder <jakub.suder@gmail.com>
2018-02-12 16:58:56 +0100

    Binance API transaction import works

--- lib/coinsync/importers/binance_api.rb
+++ lib/coinsync/importers/binance_api.rb
@@ -14,11 +14,43 @@ module CoinSync
       register_as :binance_api
 
       BASE_URL = "https://api.binance.com/api"
+      BASE_COINS = ['BTC', 'ETH', 'BNB', 'USDT']
+
+      class HistoryEntry
+        attr_accessor :quantity, :commission, :commission_asset, :price, :time, :buyer, :asset, :currency
+
+        def initialize(hash)
+          @quantity = BigDecimal.new(hash['qty'])
+          @commission = BigDecimal.new(hash['commission'])
+          @commission_asset = CryptoCurrency.new(hash['commissionAsset'])
+          @price = BigDecimal.new(hash['price'])
+          @time = Time.at(hash['time'] / 1000)
+          @buyer = hash['isBuyer']
+
+          @asset, @currency = parse_coins(hash['symbol'])
+
+          if (@buyer && @commission_asset != @asset) || (!@buyer && @commission_asset != @currency)
+            raise "Binance API: Unexpected fee: #{hash}"
+          end
+        end
+
+        def parse_coins(symbol)
+          BASE_COINS.each do |coin|
+            if symbol.end_with?(coin)
+              asset = symbol.gsub(/#{coin}$/, '')
+              return [CryptoCurrency.new(asset), CryptoCurrency.new(coin)]
+            end
+          end
+
+          raise "Binance API: Unexpected trade symbol: #{symbol}"
+        end
+      end
 
       def initialize(config, params = {})
         super
         @api_key = params['api_key']
         @secret_key = params['secret_key']
+        @traded_pairs = params['traded_pairs']
       end
 
       def can_import?
@@ -26,7 +58,32 @@ module CoinSync
       end
 
       def import_transactions(filename)
-        # TODO
+        @traded_pairs or raise "Please add a traded_pairs parameter"
+
+        transactions = []
+
+        @traded_pairs.uniq.each do |pair|
+          response = make_request('/v3/myTrades', limit: 500, symbol: pair) # TODO: paging
+
+          case response
+          when Net::HTTPSuccess
+            json = JSON.parse(response.body)
+
+            if json.is_a?(Hash)
+              raise "Kucoin importer: Invalid response: #{response.body}"
+            end
+
+            json.each { |tx| tx['symbol'] = pair }
+
+            transactions.concat(json)
+          when Net::HTTPBadRequest
+            raise "Kucoin importer: Bad request: #{response} (#{response.body})"
+          else
+            raise "Kucoin importer: Bad response: #{response}"
+          end
+        end
+
+        File.write(filename, JSON.pretty_generate(transactions.sort_by { |tx| tx['time'] }))
       end
 
       def import_balances
@@ -57,7 +114,34 @@ module CoinSync
       end
 
       def read_transaction_list(source)
-        # TODO
+        json = JSON.parse(source.read)
+        transactions = []
+
+        json.each do |hash|
+          entry = HistoryEntry.new(hash)
+
+          if entry.buyer
+            transactions << Transaction.new(
+              exchange: 'Binance',
+              time: entry.time,
+              bought_amount: entry.quantity - entry.commission,
+              bought_currency: entry.asset,
+              sold_amount: entry.price * entry.quantity,
+              sold_currency: entry.currency
+            )
+          else
+            transactions << Transaction.new(
+              exchange: 'Binance',
+              time: entry.time,
+              bought_amount: entry.price * entry.quantity - entry.commission,
+              bought_currency: entry.currency,
+              sold_amount: entry.quantity,
+              sold_currency: entry.asset
+            )
+          end
+        end
+
+        transactions
       end
 
       private
