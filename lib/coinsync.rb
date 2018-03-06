2ac8190161d87f62524f285daadd7a731c78ce2e
Kuba Suder <jakub.suder@gmail.com>
2018-03-06 14:11:25 +0200

    added some missing imports to the entry point file

--- lib/coinsync.rb
+++ lib/coinsync.rb
@@ -1,14 +1,17 @@
+require "coinsync/balance"
 require "coinsync/balance_task"
-require "coinsync/builder"
 require "coinsync/build_task"
+require "coinsync/builder"
 require "coinsync/config"
+require "coinsync/crypto_classifier"
 require "coinsync/currencies"
 require "coinsync/currency_converter"
 require "coinsync/formatter"
 require "coinsync/import_task"
+require "coinsync/request"
+require "coinsync/source"
 require "coinsync/transaction"
 require "coinsync/version"
 
 module CoinSync
-  # Your code goes here...
 end
